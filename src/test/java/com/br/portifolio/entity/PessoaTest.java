9de209db77e21959a5b748469a1d4428e499e184
rcmertz <rcliberalli@hotmail.com>
2022-09-17 17:29:00 -0300

    TestNomeNotNull

--- src/test/java/com/br/portifolio/entity/PessoaTest.java
+++ src/test/java/com/br/portifolio/entity/PessoaTest.java
@@ -1,5 +1,6 @@
 package com.br.portifolio.entity;
 
+import com.br.portifolio.service.PessoaService;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,11 +12,20 @@ public class PessoaTest {
     public void PessoaNomeTest() {
         Pessoa pessoa = new Pessoa();
 
-        pessoa.setNome("ricardo");
+        pessoa.setNome("");
 
         assertEquals("ricardo", pessoa.getNome());
         assertTrue(pessoa.toString().contains(("Pessoa{")));
 
+    }
+
+    @Test
+    public void isNomeNotNull() {
+        Pessoa pessoa = new Pessoa();
+        PessoaService pessoaService = new PessoaService();
+
+        pessoa.setNome("");
+        assertTrue(pessoaService.isNomeNotNull(pessoa));
     }
 
 }
